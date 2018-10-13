33387a24d65ecf88c1a619316a947e1d59a9b263
MassielCopara <40325446+MassielCopara@users.noreply.github.com>
2018-10-13 10:32:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,16 +16,32 @@ void bienvenida(){
 }
 
 void menuDelPorgrama(){
+  int opcion;
   cout<<"**ESTAMOS LISTOS PARA EMPEZAR A JUGAR*"<<endl<<endl;
   cout<<"Ingrese el número de la opción que desee utilizar: "<<endl;
   cout<<"(1) Jugar a completar Refranes"<<endl <<"(2) Mostrar resultados anteriores"<< endl << "(3) Salir"<<endl;
+  cout << "¿Cuál eliges?: "; cin >> opcion;
 }
 
+ void MostrarRefranIncompleto(){
+
+   //Opción 1: Justar todos los refranes incompletos un un string
+   	string refranes[2] = {"Blue askf jfjfjf ", "La comida es "};
+    for (int i = 0; i < 4; i++) 
+		cout << refranes[i] << "\n"; 
+
+     //Opción 2: Cada refrán en un string para poder imprimir la mitad y luego el refrán completo 
+         string refran = "Yo Puedo domar al Jose :v";
+    cout << refran.substr(1, 5) <<endl; 
+	
+ }
 
 //comando principal
 int main()
 {
 bienvenida();
 menuDelPorgrama();
+  if (opcion == "1")
+    MostrarRefranIncompleto()
 return 0;
 }
