b7ab39f598c5dd32844fb77665b3188710dca0e7
MassielCopara <40325446+MassielCopara@users.noreply.github.com>
2018-10-16 00:47:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 //JOOOOOOOSSSSSEEEEEEE
 
 using namespace std;
+ int opcion;
 //funciones
 void bienvenida(){
   cout<<"**************************************"<<endl;
@@ -16,7 +17,7 @@ void bienvenida(){
 }
 
 void menuDelPorgrama(){
-  int opcion;
+   
   cout<<"**ESTAMOS LISTOS PARA EMPEZAR A JUGAR*"<<endl<<endl;
   cout<<"Ingrese el número de la opción que desee utilizar: "<<endl;
   cout<<"(1) Jugar a completar Refranes"<<endl <<"(2) Mostrar resultados anteriores"<< endl << "(3) Salir"<<endl;
@@ -25,23 +26,37 @@ void menuDelPorgrama(){
 
  void MostrarRefranIncompleto(){
 
-   //Opción 1: Justar todos los refranes incompletos un un string
-   	string refranes[2] = {"Blue askf jfjfjf ", "La comida es "};
-    for (int i = 0; i < 4; i++) 
-		cout << refranes[i] << "\n"; 
+   //Opción 1: Justar todos los refranes incompletos un un string 
+   string respuesta;
+   cout << "\n VAMOS CON LOS REFRANES! \n ";
+   	string refranes[2] = {"Vamos a domar al Jose ", "La reina Chunga se pasa Nico se pasa"};
+    for (int i = 0; i < 2; i++) {
+    string refran = refranes[i];
+    cout << refran.substr(0, 15) << "[...]" << endl;
+    cout <<"Completa el refrán" << endl; cin >> respuesta;
+    string correcto = refran.substr(15, -1);
+    if (respuesta = correcto)){
+      cout << "OMG OMG OMG! GOOOOD!";
+    } 
+ 
 
-     //Opción 2: Cada refrán en un string para poder imprimir la mitad y luego el refrán completo 
-         string refran = "Yo Puedo domar al Jose :v";
-    cout << refran.substr(1, 5) <<endl; 
-	
- }
+      	
+ }}
 
 //comando principal
-int main()
-{
+int main(){
 bienvenida();
 menuDelPorgrama();
-  if (opcion == "1")
-    MostrarRefranIncompleto()
+  switch (opcion){
+case 1: cout << "Usted ha seleccionado la opción 1"<< endl;MostrarRefranIncompleto();
+    break;
+    case 2: cout << "Usted ha seleccionado la opción 2";
+    break;
+    case 3: cout << "Usted ha seleccionado la opción 3";
+    break;
+    default: cout << "Usted ha ingresado una opción incorrecta";
+  }
+    
 return 0;
+}
 }
